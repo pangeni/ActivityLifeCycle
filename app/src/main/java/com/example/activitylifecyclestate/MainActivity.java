e0ce5a85a5e171f1695b802fa899a9dbda74b62f
pangeni <pangenisanjay@gmail.com>
2022-04-02 15:01:21 +0545

    Activity Life Cycle

--- app/src/main/java/com/example/activitylifecyclestate/MainActivity.java
+++ app/src/main/java/com/example/activitylifecyclestate/MainActivity.java
@@ -112,4 +112,39 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
 }
