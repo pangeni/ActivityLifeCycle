701eaac3118e8393e15188994954a4f14ed8bd11
pangeni <pangenisanjay@gmail.com>
2022-04-02 15:24:40 +0545

    CodingChallenge

--- app/src/main/java/com/example/activitylifecyclestate/MainActivity.java
+++ app/src/main/java/com/example/activitylifecyclestate/MainActivity.java
@@ -1,86 +1,73 @@
 package com.example.activitylifecyclestate;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
 import android.content.Intent;
-import android.util.Log;
+import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
-    // Class name for Log tag
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
-    // Unique tag required for the intent extra
-    public static final String EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE";
-    // Unique tag for the intent reply
+
     public static final int TEXT_REQUEST = 1;
+    private ShopList items = new ShopList();
+    public MainActivity() {
+    }
 
-    // EditText view for the message
-    private EditText mMessageEditText;
-    // TextView for the reply header
-    private TextView mReplyHeadTextView;
-    // TextView for the reply body
-    private TextView mReplyTextView;
-
-    /**
-     * Initializes the activity.
-     *
-     * @param savedInstanceState The current state data.
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize all the view variables.
-        mMessageEditText = (EditText) findViewById(R.id.editText_main);
-        mReplyHeadTextView = (TextView) findViewById(R.id.text_header_reply);
-        mReplyTextView = (TextView) findViewById(R.id.text_message_reply);
+        Intent intent = getIntent();
+        if ((savedInstanceState != null)&& (savedInstanceState.getSerializable("list")!= null)){
+            HashMap<String, Integer> l = (HashMap<String, Integer>)savedInstanceState.getSerializable("list");
+            TextView tv = findViewById(R.id.textView);
+            tv.setText("");
+            for (String k : l.keySet()) {
+                String s = l.get(k).toString() + " " + k + "\n";
+                tv.setText(tv.getText() + s);
+                for(int i = 0; i<l.get(k); i++){
+                    items.addItem(k);}
+            }
+        }
     }
 
-    /**
-     * Handle the onClick for the "Send" button. Gets the value of the main EditText,
-     * creates an intent, and launches the second activity with that intent.
-     *
-     * The return intent from the second activity is onActivityResult().
-     * @param view The view (Button) that was clicked.
-     */
     public void launchSecondActivity(View view) {
-        Log.d(LOG_TAG, "Button clicked!");
 
         Intent intent = new Intent(this, SecondActivity.class);
-        String message = mMessageEditText.getText().toString();
 
-        intent.putExtra(EXTRA_MESSAGE, message);
         startActivityForResult(intent, TEXT_REQUEST);
     }
 
-    /**
-     * Handle the data in the return intent from SecondActivity.
-     *
-     * @param requestCode Code for the SecondActivity request.
-     * @param resultCode Code that comes back from SecondActivity.
-     * @param data Intent data sent back from SecondActivity.
-     */
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putSerializable("list", items.getItems());
+    }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        // Test for the right intent reply
         if (requestCode == TEXT_REQUEST) {
-            // Test to make sure the intent reply result was good.
             if (resultCode == RESULT_OK) {
-                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
-
-                // Make the reply head visible.
-                mReplyHeadTextView.setVisibility(View.VISIBLE);
-
-                // Set the reply and make it visible.
-                mReplyTextView.setText(reply);
-                mReplyTextView.setVisibility(View.VISIBLE);
+                String item = data.getStringExtra(SecondActivity.EXTRA_MESSAGE);
+                items.addItem(item);
             }
+            drawView();
+        }
+    }
+
+    public void drawView() {
+        HashMap<String, Integer> l = items.getItems();
+        TextView tv = findViewById(R.id.textView);
+        tv.setText("");
+        for (String k : l.keySet()) {
+            String s = l.get(k).toString() + " " + k + "\n";
+            tv.setText(tv.getText() + s);
         }
     }
 }
--- app/src/main/java/com/example/activitylifecyclestate/SecondActivity.java
+++ app/src/main/java/com/example/activitylifecyclestate/SecondActivity.java
@@ -3,63 +3,27 @@ package com.example.activitylifecyclestate;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-/**
- * SecondActivity defines the second activity in the app. It is launched from an intent
- * with a message, and sends an intent back with a second message.
- */
 public class SecondActivity extends AppCompatActivity {
-    // Unique tag for the intent reply.
-    public static final String EXTRA_REPLY =
-            "com.example.android.twoactivities.extra.REPLY";
 
-    // EditText for the reply.
-    private EditText mReply;
+    public static final String EXTRA_MESSAGE = "ca.nbcc.shoppinglist.extra.MESSAGE";
 
-    /**
-     * Initializes the activity.
-     *
-     * @param savedInstanceState The current state data
-     */
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
-        // Initialize view variables.
-        mReply = (EditText) findViewById(R.id.editText_second);
-
-        // Get the intent that launched this activity, and the message in
-        // the intent extra.
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-
-        // Put that message into the text_message TextView
-        TextView textView = (TextView) findViewById(R.id.text_message);
-        if (textView != null) {
-            textView.setText(message);
-        }
     }
 
-    /**
-     * Handle the onClick for the "Reply" button. Gets the message from the second EditText,
-     * creates an intent, and returns that message back to the main activity.
-     *
-     * @param view The view (Button) that was clicked.
-     */
-    public void returnReply(View view) {
-        // Get the reply message from the edit text.
-        String reply = mReply.getText().toString();
-
-        // Create a new intent for the reply, add the reply message to it as an extra,
-        // set the intent result, and close the activity.
-        Intent replyIntent = new Intent();
-        replyIntent.putExtra(EXTRA_REPLY, reply);
-        setResult(RESULT_OK, replyIntent);
+    public void addItem (View view) {
+        Intent replyIntent=new Intent();
+        String message=((Button)view).getText().toString();
+        replyIntent.putExtra(EXTRA_MESSAGE, message);
+        setResult(RESULT_OK,replyIntent);
         finish();
     }
 }
