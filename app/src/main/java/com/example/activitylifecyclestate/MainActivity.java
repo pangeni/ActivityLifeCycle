05b1682c9e0650061e308b317084c4e09b3f3109
pangeni <pangenisanjay@gmail.com>
2022-04-04 20:45:12 +0545

    Homework Done

--- app/src/main/java/com/example/activitylifecyclestate/MainActivity.java
+++ app/src/main/java/com/example/activitylifecyclestate/MainActivity.java
@@ -1,20 +1,27 @@
 package com.example.activitylifecyclestate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private int mCount = 0;
     private TextView mShowCount;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.view_count);
+        if (savedInstanceState != null) {
+            mCount = savedInstanceState.getInt("count");
+            mShowCount.setText(""+ mCount);
+        }
 
     }
     public void countUp(View view) {
@@ -23,14 +30,20 @@ public class MainActivity extends AppCompatActivity {
             mShowCount.setText(Integer.toString(mCount));
     }
     @Override
-    public void onSaveInstanceState(Bundle outState) {
+    public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        if (mShowCount.getVisibility() == View.VISIBLE) {
-            outState.putBoolean("reply_visible", true);
-        }
-
+        EditText text = findViewById(R.id.EditText);
+        CharSequence data = text.getText();
+        outState.putCharSequence("data", data );
+        outState.putInt("count",mCount);
 
     }
 
-
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        CharSequence data = savedInstanceState.getCharSequence("data");
+        EditText text = findViewById(R.id.EditText);
+        text.setText(data);
+    }
 }
